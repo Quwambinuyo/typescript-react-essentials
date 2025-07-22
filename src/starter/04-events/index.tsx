374570b359d30877133e3c6b5941802409256593
Quwam Binuyo <qbinuyo7@gmail.com>
2025-07-22 11:03:51 +0100

    Update Component to handle form submission and input state management

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,58 @@
+import { FormEventHandler, useState } from "react";
+
+interface Person {
+  name: string;
+}
+
 function Component() {
+  const [text, setText] = useState("");
+  const [email, setEmailt] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmailt(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    // const formData = new FormData(e.target as HTMLFormElement)
+
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const text = formData.get("text") as string;
+    const person: Person = {
+      name: text,
+    };
+  };
+
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
+    <section>
+      <div>
+        <h2>Events Example</h2>
+        <form onSubmit={handleSubmit} className="form">
+          <input
+            type="text"
+            className="form-input mb-1"
+            value={text}
+            name="text"
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="email"
+            className="form-input mb-1"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
 export default Component;
