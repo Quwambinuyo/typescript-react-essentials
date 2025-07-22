2112d32d388b1c6bc1c95ab873bf864c3e9116fe
Quwam Binuyo <qbinuyo7@gmail.com>
2025-07-22 10:04:01 +0100

    Refactor Component to use TypeScript props and update App to render it correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Component from "./starter/02-props";
+
 function App() {
   return (
     <main>
       <h2>React & Typescript</h2>
+      <Component name="peter" id={123}>
+        <h1>Hello</h1>
+      </Component>
     </main>
   );
 }
--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,8 +1,15 @@
-function Component() {
+interface ComponentProps {
+  name: string;
+  id: number;
+  children?: React.ReactNode;
+}
+
+function Component({ name, id, children }: ComponentProps) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h2>Name: {name}</h2>
+      <h2>Id: {id}</h2>
+      {children && <div>{children}</div>}
     </div>
   );
 }
