17d6d55cdda326ceecb3c6ea235b92022c8c1cf2
Quwam Binuyo <qbinuyo7@gmail.com>
2025-07-28 13:25:34 +0100

    Update Component to define ProfileCardProps and handle basic and advanced user profiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Component from "./starter/04-events";
+import Component from "./starter/05-challenge";
 
 function App() {
   return (
     <main>
-      <Component />
+      {/* <Component name="anna" type="advanced" email="anna@gmail.com" />
+      <Component name="anna" type="basic" email="anna@gmail.com" /> */}
+      <Component name="anna" type="advanced" email="anna@gmail.com" />
     </main>
   );
 }
--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,9 +1,38 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
-  );
+interface BasicProfileCardProps {
+  type: "basic";
+  name: string;
+  // email: string;
+}
+interface AdvancedProfileCardProps {
+  type: "advanced";
+  name: string;
+  email: string;
+}
+
+type ProfileCardProps = BasicProfileCardProps | AdvancedProfileCardProps;
+
+function Component(props: ProfileCardProps) {
+  // const { type, name, email } = props;
+  // const alertType = type === "basic" ? "success" : "danger";
+  // const className = `alert alert-${alertType}`;
+
+  const { type, name } = props;
+  if (type === "basic") {
+    return (
+      <article className="alert alert-success">
+        <h2>User = {name}</h2>
+      </article>
+    );
+  }
+
+  const { email } = props;
+  if (type === "advanced") {
+    return (
+      <article className="alert alert-danger">
+        <h2>User = {name}</h2>
+        <h2>Email = {email}</h2>
+      </article>
+    );
+  }
 }
 export default Component;
